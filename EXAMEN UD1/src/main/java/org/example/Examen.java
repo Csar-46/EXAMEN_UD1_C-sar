79fbcf415e421c5704859189f9590d59aa755e77
A603\1DAW <septiembrecesar@hotmail.com>
2024-10-04 19:04:47 +0200

    EXAMEN UD1

--- EXAMEN UD1/src/main/java/org/example/Examen.java
+++ EXAMEN UD1/src/main/java/org/example/Examen.java
@@ -7,14 +7,15 @@ public class Examen {
 
     public void ejercicio1() {
 
-        boolean error = true;
+        boolean error = true;  //Inicializo las variables para que entren si o si en la primera iteración del bucle.
         int num1 = -1;
         int num2 = -1;
         int num3 = -1;
         float media;
 
         Scanner entrada = new Scanner(System.in);
-        while (error == true) { //Controlamos que el valor del multiplicador sea numerico
+
+        while (error == true) { //Controlamos que el valor de las notaas sea numerico con el bucle y el try-catch
             try {
                 while (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10 || num3 < 0 || num3 > 10) {
 
@@ -30,25 +31,25 @@ public class Examen {
                     }
                 }
 
-                error = false; //cambiamos el valor para que no vuelva a entrar al bucle.
+                error = false; //Cambiamos el valor para que no vuelva a entrar al bucle.
 
-            } catch (InputMismatchException er) {
+            } catch (InputMismatchException er) { //Es caso de introducir un valor no numerico el catch imprimira el mensaje indicado
 
                 System.out.println("ERROR. Introduce un valor numérico:");
                 entrada.nextLine();
 
             }
 
-            media = num1 + num2 + num3;
-            media = media / 3;
-            System.out.println("La media obtenida es " + media);
-
-            if (media >= 5) {
-                System.out.println("APROBADO.");
-            } else
-                System.out.println("SUSPENDIDO.");
 
         }
+        media = num1 + num2 + num3;     //Con estas dos operaciones se hace la media de los trimestres.
+        media = media / 3;
+        System.out.println("La media obtenida es " + media);
+
+        if (media >= 5) {  //Con este condicional comprobamos si la nota es mayor o menor de 5 para saber si esta APROBADO o SUSPENDIDO.
+            System.out.println("APROBADO.");
+        } else
+            System.out.println("SUSPENDIDO.");
 
     }
 
@@ -60,7 +61,7 @@ public class Examen {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce un numero...");
 
-        while (error == true) { //Controlamos que el valor del multiplicador sea numerico
+        while (error == true) { //Controlamos que el valor del numero no sea una letra
 
             try {
                 num = entrada.nextInt();
@@ -74,13 +75,12 @@ public class Examen {
 
             }
 
-            if (num >= 0){
-                System.out.println("El valor absoluto es " + num);
-            }else{
-                num = num + (2*(-num));
-                System.out.println("El valor absoluto es " + num);
-            }
-
+        }
+        if (num >= 0){ //Con la condicion vemos si es positivo o negativo
+            System.out.println("El valor absoluto es " + num);
+        }else{
+            num = num + (2*(-num)); //Con esta operacion cambio el valor de negativo a positivo para que imprima el valor absoluto
+            System.out.println("El valor absoluto es " + num);
         }
     }
 }
